ebb93b63d42d5fa831b5cb2ccdea3fa08b54d57c
Ivan Mendoza <theeachtra2002@yahoo.com>
2019-05-08 18:31:10 -0500

    adding localhost to server.js

--- server.js
+++ server.js
@@ -18,8 +18,7 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://<dbuser>:<dbpassword>@ds153566.mlab.com:53566/heroku_927jlqvr",
-  {
+  process.env.MONGODB_URI || "mongodb://localhost/googlebooks",  {
     useCreateIndex: true,
     useNewUrlParser: true
   }
